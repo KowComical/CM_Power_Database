07f2ced089829b301a5221dc815eebc24a697ef5
KowComical <xuanren1992@yahoo.com>
2022-04-23 00:43:34 +0800

    test

--- code/global_code/global_function.py
+++ code/global_code/global_function.py
@@ -144,6 +144,8 @@ def get_yesterday():
 
 def search_file(file_path):
     import os
+    import sys
+    sys.dont_write_bytecode = True
     file_name = []
     for parent, surnames, filenames in os.walk(file_path):
         for fn in filenames:
