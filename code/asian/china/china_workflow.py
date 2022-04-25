88f457f2c469977c2d1426a5e0563d2bf6f65e08
KowComical <xuanren1992@yahoo.com>
2022-04-25 14:54:30 +0800

    test

--- code/asian/china/china_workflow.py
+++ code/asian/china/china_workflow.py
@@ -6,7 +6,7 @@ import functools
 import numpy as np
 from datetime import datetime
 import sys
-
+sys.dont_write_bytecode = True
 sys.path.append('./code/global_code/')
 import global_function as af
 import global_all as g
