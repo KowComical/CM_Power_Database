2ad3a401bc82cb0c2ef59ede97eab9dd7724d3b2
KowComical <xuanren1992@yahoo.com>
2022-04-26 13:54:40 +0800

    update

--- code/global_code/global_sum.py
+++ code/global_code/global_sum.py
@@ -57,51 +57,58 @@ df_all['country'] = df_all['country'].str.replace('United_kingdom_bmrs', 'United
 df_all['country'] = df_all['country'].str.replace('Bosnia and Herz', 'Bosnia & Herz')
 df_all['country'] = df_all['country'].str.replace('Us', 'United States')
 
-col_list = ['date', 'year', 'country', 'Type', 'Value']
-yesterday = af.get_yesterday().strftime('%Y-%m-%d')
-df_all = df_all[df_all['date'] < yesterday].reset_index(drop=True)
-df_all['year'] = df_all['date'].dt.year
+# col_list = ['date', 'year', 'country', 'Type', 'Value']
+# yesterday = af.get_yesterday().strftime('%Y-%m-%d')
+# df_all = df_all[df_all['date'] < yesterday].reset_index(drop=True)
+# df_all['year'] = df_all['date'].dt.year
 df_all = pd.pivot_table(df_all, index=['unit', 'date', 'year', 'month', 'month_date', 'weekday', 'country'],
                         values='Value', columns='Type').reset_index()
-
-df_all = df_all[
-    (df_all['year'] == 2021) & (df_all['month'] == 12)].reset_index(drop=True)
-
+#
+# df_all = df_all[
+#     (df_all['year'] == 2021) & (df_all['month'] == 12)].reset_index(drop=True)
+#
 df_all = df_all[['country', 'date', 'coal', 'gas', 'oil', 'nuclear', 'hydro', 'solar', 'wind', 'other']]
 df_all = df_all.set_index(['country', 'date']).stack().reset_index().rename(
     columns={'Type': 'energy_source', 0: 'value', 'country': 'country/region'})
-
+#
 # 添加欧盟
-df_c = pd.read_csv(global_path+'EU_country_list.csv')
+df_c = pd.read_csv(global_path + 'EU_country_list.csv')
 eu27_list = df_c['country'].tolist() + ['United Kingdom']
 df_eu27 = df_all[df_all['country/region'].isin(eu27_list)].groupby(
     ['date', 'energy_source']).sum().reset_index()
 df_eu27['country/region'] = 'EU27&UK'
 df_all = pd.concat([df_all, df_eu27]).reset_index(drop=True)
-# 只要6个国家
-country_list = ['India', 'United States', 'EU27&UK', 'United Kingdom', 'Brazil', 'China', 'Japan']
-df_all = df_all[df_all['country/region'].isin(country_list)].reset_index(drop=True)
-# df_t = df_all.groupby(['date', 'energy_source']).sum().reset_index()
-# df_t['country/region'] = 'Total'
-# df_all = pd.concat([df_all, df_t])
 
-time_stamp = []  # 将当地时间转换为时间戳
-for d in df_all['date'].tolist():
-    time_stamp.append(time.mktime(d.timetuple()))
-df_all['timestamp'] = time_stamp
-
-utc_time = []  # 将当地时间时间戳转换为utc时间
-for t in df_all['timestamp'].tolist():
-    utc_time.append(datetime.fromtimestamp(t, tz=timezone.utc))
-df_all['utc'] = utc_time
-
-time_stamp = []  # 将utc时间转换为utc时间戳
-for d in df_all['utc'].tolist():
-    time_stamp.append(time.mktime(d.timetuple()))
-df_all['timestamp'] = time_stamp
-df_all = df_all[['country/region', 'date', 'energy_source', 'value', 'timestamp']]
-df_all['date'] = df_all['date'].dt.strftime('%d/%m/%Y')
-
+df_all = df_all.groupby(['country/region', 'date']).sum().reset_index()
+df_all = pd.pivot_table(df_all, index='date', values='value', columns='country/region').reset_index()
+df_all = df_all[
+    ['date', 'Brazil', 'China', 'EU27&UK', 'France', 'Germany', 'India', 'Italy', 'Japan', 'Spain',
+     'United Kingdom', 'United States']]
+df_all = df_all[(df_all['date'] >= '2019-01-01') & (df_all['date'] < '2022-04-01')].reset_index(drop=True)
+# # 只要6个国家
+# country_list = ['India', 'United States', 'EU27&UK', 'United Kingdom', 'Brazil', 'China', 'Japan']
+# df_all = df_all[df_all['country/region'].isin(country_list)].reset_index(drop=True)
+# # df_t = df_all.groupby(['date', 'energy_source']).sum().reset_index()
+# # df_t['country/region'] = 'Total'
+# # df_all = pd.concat([df_all, df_t])
+#
+# time_stamp = []  # 将当地时间转换为时间戳
+# for d in df_all['date'].tolist():
+#     time_stamp.append(time.mktime(d.timetuple()))
+# df_all['timestamp'] = time_stamp
+#
+# utc_time = []  # 将当地时间时间戳转换为utc时间
+# for t in df_all['timestamp'].tolist():
+#     utc_time.append(datetime.fromtimestamp(t, tz=timezone.utc))
+# df_all['utc'] = utc_time
+#
+# time_stamp = []  # 将utc时间转换为utc时间戳
+# for d in df_all['utc'].tolist():
+#     time_stamp.append(time.mktime(d.timetuple()))
+# df_all['timestamp'] = time_stamp
+# df_all = df_all[['country/region', 'date', 'energy_source', 'value', 'timestamp']]
+# df_all['date'] = df_all['date'].dt.strftime('%d/%m/%Y')
+#
 df_all.to_csv(
-    global_path + 'Global_PM_corT_2021.12.csv', index=False,
+    global_path + 'Global_Power_2022.4.26.csv', index=False,
     encoding='utf_8_sig')
