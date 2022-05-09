e0705c4905e9f6711035befb0a3b5decb7467fa4
KowComical <xuanren1992@yahoo.com>
2022-05-09 18:29:53 +0800

    update

--- code/global_code/test.py
+++ code/global_code/test.py
@@ -2,30 +2,36 @@ import warnings
 warnings.filterwarnings('ignore')
 import sys
 sys.dont_write_bytecode = True
+
 sys.path.append('./code/asian/china/')
-# Begin process China...
+print('Begin process China...')
 import china_workflow
 
 sys.path.append('./code/asian/india/')
-# Begin process India...
+print('Begin process India...')
 import india_workflow as i
 i.main()
+
 sys.path.append('./code/asian/japan/')
-# Begin process japan...
+print('Begin process japan...')
 import japan_workflow as j
 j.main()
+
 sys.path.append('./code/europe/eu27_uk/')
-# Begin process EU...
+print('Begin process EU...')
 import eu_workflow as e
 e.main()
+
 sys.path.append('./code/europe/russia/')
-# Begin process Russia...
+print('Begin process Russia...')
 import russia_workflow
+
 sys.path.append('./code/n_america/us/')
-# Begin process US...
+print('Begin process US...')
 import us_workflow
+
 sys.path.append('./code/s_america/brazil/')
+print('Begin process Brazil...')
 import brazil_workflow as b
-# Begin process Brazil...
 b.main()
 
