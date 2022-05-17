9cb0836a48b9d97306f0ea16d71d3d5c089bed3f
KowComical <xuanren1992@yahoo.com>
2022-05-17 11:03:48 +0800

    update readme

--- code/asian/china/china_workflow.py
+++ code/asian/china/china_workflow.py
@@ -7,7 +7,7 @@ import numpy as np
 import sys
 
 sys.dont_write_bytecode = True
-sys.path.append('./code/global_code/')
+sys.path.append('./code/')
 from global_code import global_function as af
 from global_code import global_all as g
 
