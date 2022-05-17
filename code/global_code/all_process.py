42a55d377152cf5daa5ae3e4453379543afdda92
KowComical <xuanren1992@yahoo.com>
2022-05-17 11:43:25 +0800

    update

--- code/global_code/all_process.py
+++ code/global_code/all_process.py
@@ -3,38 +3,61 @@ warnings.filterwarnings('ignore')
 import sys
 sys.dont_write_bytecode = True
 
-sys.path.append('./code/asian/china/')
-print('Begin process China...')
-import china_workflow
+try:
+    sys.path.append('./code/asian/china/')
+    print('Begin process China...')
+    import china_workflow
+except Exception as e:
+    print(e)
 
-sys.path.append('./code/asian/india/')
-print('Begin process India...')
-import india_workflow as i
-i.main()
+try:
+    sys.path.append('./code/asian/india/')
+    print('Begin process India...')
+    import india_workflow as i
+    i.main()
+except Exception as e:
+    print(e)
 
-sys.path.append('./code/asian/japan/')
-print('Begin process Japan...')
-import japan_workflow as j
-j.main()
+try:
+    sys.path.append('./code/asian/japan/')
+    print('Begin process Japan...')
+    import japan_workflow as j
+    j.main()
+except Exception as e:
+    print(e)
 
-sys.path.append('./code/europe/eu27_uk/')
-print('Begin process EU...')
-import eu_workflow as e
-e.main()
+try:
+    sys.path.append('./code/europe/eu27_uk/')
+    print('Begin process EU...')
+    import eu_workflow as e
+    e.main()
+except Exception as e:
+    print(e)
 
-sys.path.append('./code/europe/russia/')
-print('Begin process Russia...')
-import russia_workflow
+try:
+    sys.path.append('./code/europe/russia/')
+    print('Begin process Russia...')
+    import russia_workflow
+except Exception as e:
+    print(e)
 
-sys.path.append('./code/n_america/us/')
-print('Begin process US...')
-import us_workflow
+try:
+    sys.path.append('./code/n_america/us/')
+    print('Begin process US...')
+    import us_workflow
+except Exception as e:
+    print(e)
 
-sys.path.append('./code/s_america/brazil/')
-print('Begin process Brazil...')
-import brazil_workflow as b
-b.main()
-
-import iea_workflow
+try:
+    sys.path.append('./code/s_america/brazil/')
+    print('Begin process Brazil...')
+    import brazil_workflow as b
+    b.main()
+except Exception as e:
+    print(e)
+try:
+    import iea_workflow
+except Exception as e:
+    print(e)
 import cal_ef_emission
 import draw_pic
