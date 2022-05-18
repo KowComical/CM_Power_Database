01c823542abc66bb38015493cae3863313ed73ac
KowComical <xuanren1992@yahoo.com>
2022-05-18 12:02:38 +0800

    update draw_pic

--- code/global_code/all_process.py
+++ code/global_code/all_process.py
@@ -1,63 +1,76 @@
 import warnings
+
 warnings.filterwarnings('ignore')
 import sys
+
 sys.dont_write_bytecode = True
+#
+# try:
+#     sys.path.append('./code/asian/china/')
+#     print('Begin process China...')
+#     import china_workflow
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/asian/india/')
+#     print('Begin process India...')
+#     import india_workflow as i
+#     i.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/asian/japan/')
+#     print('Begin process Japan...')
+#     import japan_workflow as j
+#     j.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/europe/eu27_uk/')
+#     print('Begin process EU...')
+#     import eu_workflow as e
+#     e.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/europe/russia/')
+#     print('Begin process Russia...')
+#     import russia_workflow
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/n_america/us/')
+#     print('Begin process US...')
+#     import us_workflow
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/s_america/brazil/')
+#     print('Begin process Brazil...')
+#     import brazil_workflow as b
+#     b.main()
+# except Exception as e:
+#     print(e)
+# try:
+#     import iea_workflow
+# except Exception as e:
+#     print(e)
+# import cal_ef_emission
 
-try:
-    sys.path.append('./code/asian/china/')
-    print('Begin process China...')
-    import china_workflow
-except Exception as e:
-    print(e)
+import draw_pic as d
+d.main(category='coal')
+d.main(category='gas')
+d.main(category='oil')
+d.main(category='nuclear')
+d.main(category='solar')
+d.main(category='hydro')
+d.main(category='wind')
+d.main(category='other')
+d.main(category=False)
 
-try:
-    sys.path.append('./code/asian/india/')
-    print('Begin process India...')
-    import india_workflow as i
-    i.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/asian/japan/')
-    print('Begin process Japan...')
-    import japan_workflow as j
-    j.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/europe/eu27_uk/')
-    print('Begin process EU...')
-    import eu_workflow as e
-    e.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/europe/russia/')
-    print('Begin process Russia...')
-    import russia_workflow
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/n_america/us/')
-    print('Begin process US...')
-    import us_workflow
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/s_america/brazil/')
-    print('Begin process Brazil...')
-    import brazil_workflow as b
-    b.main()
-except Exception as e:
-    print(e)
-try:
-    import iea_workflow
-except Exception as e:
-    print(e)
-import cal_ef_emission
-import draw_pic
--- code/global_code/draw_pic.py
+++ code/global_code/draw_pic.py
@@ -1,3 +1,4 @@
+from matplotlib.dates import MonthLocator, DateFormatter
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
@@ -5,100 +6,181 @@ import os
 from datetime import datetime
 
 import sys
+
 sys.dont_write_bytecode = True
 sys.path.append('./code/global_code/')
 import global_function as af
 
-file_path = './data/'
-global_path = os.path.join(file_path, 'global')
-out_path = './image/'
 
-file_name = af.search_file(file_path)
-file_name = [file_name[i] for i, x in enumerate(file_name) if x.find('simulated') != -1]
-file_name = [file_name[i] for i, x in enumerate(file_name) if x.find('daily') != -1]
-file_name_no = [file_name[i] for i, x in enumerate(file_name) if not x.find('eu27_uk') != -1]
-file_name_eu = [file_name[i] for i, x in enumerate(file_name) if x.find('eu27_uk') != -1]
-file_name_eu = [file_name_eu[i] for i, x in enumerate(file_name_eu) if not x.find('United Kingdom') != -1]
+def main(category):
+    out_path = './image/'
+    country_list = ['Brazil', 'China', 'Russia', 'EU27&UK', 'France', 'Germany', 'India', 'Italy', 'Japan', 'Spain',
+                    'United Kingdom', 'United States']  # 这里以后要修改
+    df_all = sum_country(country_list, category)  # 合并数据
 
-# # 提取主要国家名
-name = re.compile(r'data/.*?/(?P<name>.*?)/simulated', re.S)
-df_all = pd.DataFrame()
-for f in file_name_no:
-    c = name.findall(f)[0]
-    df_temp = pd.read_csv(f)
-    af.time_info(df_temp, 'date')
-    df_temp = af.check_col(df_temp, 'daily')
-    df_temp['country'] = c.capitalize()
-    df_all = pd.concat([df_all, df_temp]).reset_index(drop=True)
+    plt.style.use('seaborn-poster')  # 图表风格
+    plt.figure(figsize=(100, 50))  # 设置图表大小
 
-# 欧州国家
-eu_name = re.compile(r'daily/(?P<name>.*?).csv', re.S)
-result_eu = []
-for f in file_name_eu:
-    c = eu_name.findall(f)[0]
-    df_temp = pd.read_csv(f)
-    df_temp['country'] = c.capitalize()
-    df_all = pd.concat([df_all, df_temp]).reset_index(drop=True)
+    sub_plot(df_all, country_list)  # 开始画图
 
-for x in df_all.columns.tolist():
-    # noinspection PyBroadException
-    try:
-        df_all[x] = df_all[x].astype(float)
-    except:
-        pass
-af.time_info(df_all, 'date')
+    out_put(out_path, category)  # 输出图
 
-df_all = df_all.set_index(
-    ['unit', 'date', 'year', 'month', 'month_date', 'weekday', 'country']).stack().reset_index().rename(
-    columns={'level_7': 'Type', 0: 'Value'})
-df_all = df_all[df_all['Type'].isin(['coal', 'gas', 'oil', 'nuclear', 'hydro', 'solar', 'wind', 'other'])].reset_index(
-    drop=True)
-df_all['country'] = df_all['country'].str.replace('United_kingdom_bmrs', 'United Kingdom')
-df_all['country'] = df_all['country'].str.replace('Bosnia and Herz', 'Bosnia & Herz')
-df_all['country'] = df_all['country'].str.replace('Us', 'United States')
 
-# 前一天的数据基本都不准确
-col_list = ['date', 'year', 'country', 'Type', 'Value']
-yesterday = af.get_yesterday().strftime('%Y-%m-%d')
-df_all = df_all[df_all['date'] < yesterday].reset_index(drop=True)
-df_all['year'] = df_all['date'].dt.year
+def sum_country(country_list, category):
+    file_path = './data/'
+    global_path = os.path.join(file_path, 'global')
 
-df_all = pd.pivot_table(df_all, index=['unit', 'date', 'year', 'month', 'month_date', 'weekday', 'country'],
-                        values='Value', columns='Type').reset_index()
+    file_name = af.search_file(file_path)
+    file_name = [file_name[i] for i, x in enumerate(file_name) if x.find('simulated') != -1]
+    file_name = [file_name[i] for i, x in enumerate(file_name) if x.find('daily') != -1]
+    file_name_no = [file_name[i] for i, x in enumerate(file_name) if not x.find('eu27_uk') != -1]
+    file_name_eu = [file_name[i] for i, x in enumerate(file_name) if x.find('eu27_uk') != -1]
+    file_name_eu = [file_name_eu[i] for i, x in enumerate(file_name_eu) if not x.find('United Kingdom') != -1]
 
-df_all = df_all[['country', 'date', 'coal', 'gas', 'oil', 'nuclear', 'hydro', 'solar', 'wind', 'other']]
-df_all = df_all.set_index(['country', 'date']).stack().reset_index().rename(
-    columns={'Type': 'energy_source', 0: 'value', 'country': 'country/region'})
+    # 提取主要国家名
+    name = re.compile(r'data/.*?/(?P<name>.*?)/simulated', re.S)
 
-# 添加欧盟
-df_c = pd.read_csv(os.path.join(global_path, 'EU_country_list.csv'))
+    df_all = pd.DataFrame()
+    for f in file_name_no:
+        c = name.findall(f)[0]
+        df_temp = pd.read_csv(f)
+        af.time_info(df_temp, 'date')
+        df_temp = af.check_col(df_temp, 'daily')
+        df_temp['country'] = c.capitalize()
+        df_all = pd.concat([df_all, df_temp]).reset_index(drop=True)
 
-eu27_list = df_c['country'].tolist() + ['United Kingdom']
-df_eu27 = df_all[df_all['country/region'].isin(eu27_list)].groupby(
-    ['date', 'energy_source']).sum().reset_index()
-df_eu27['country/region'] = 'EU27&UK'
-df_all = pd.concat([df_all, df_eu27]).reset_index(drop=True)
+    # 欧州国家
+    eu_name = re.compile(r'daily/(?P<name>.*?).csv', re.S)
+    for f in file_name_eu:
+        c = eu_name.findall(f)[0]
+        df_temp = pd.read_csv(f)
+        df_temp['country'] = c.capitalize()
+        df_all = pd.concat([df_all, df_temp]).reset_index(drop=True)
 
-df_all = df_all.groupby(['country/region', 'date']).sum().reset_index()
-df_all = pd.pivot_table(df_all, index='date', values='value', columns='country/region').reset_index()
-country_list = ['Brazil', 'China', 'Russia', 'EU27&UK', 'France', 'Germany', 'India', 'Italy', 'Japan', 'Spain',
-                'United Kingdom', 'United States']
-df_all = df_all[['date'] + country_list]
-df_all = df_all[df_all['date'] >= '2019-01-01'].reset_index(drop=True)
-df_all['year'] = df_all['date'].dt.year
-df_all['month'] = df_all['date'].dt.month
+    for x in df_all.columns.tolist():
+        # noinspection PyBroadException
+        try:
+            df_all[x] = df_all[x].astype(float)
+        except:
+            pass
+    af.time_info(df_all, 'date')
 
-plt.style.use('seaborn-poster')
-plt.figure(figsize=(100, 50))
-for i in range(len(country_list)):
-    pic = plt.subplot(3, 4, i + 1)
-    af.draw_pic(df_all, country_list[i], i)
-plt.tight_layout()
+    df_all = df_all.set_index(
+        ['unit', 'date', 'year', 'month', 'month_date', 'weekday', 'country']).stack().reset_index().rename(
+        columns={'level_7': 'Type', 0: 'Value'})
+    df_all = df_all[
+        df_all['Type'].isin(['coal', 'gas', 'oil', 'nuclear', 'hydro', 'solar', 'wind', 'other'])].reset_index(
+        drop=True)
+    df_all['country'] = df_all['country'].str.replace('United_kingdom_bmrs', 'United Kingdom')
+    df_all['country'] = df_all['country'].str.replace('Bosnia and Herz', 'Bosnia & Herz')
+    df_all['country'] = df_all['country'].str.replace('Us', 'United States')
 
-# 建立储存图片的分年份月份文件夹
-current_date = datetime.now().strftime('%Y%m%d')
-out_path_yearly = af.create_folder(out_path, current_date[:4])
-out_path_monthly = af.create_folder(out_path_yearly, current_date[4:6])
-plt.savefig(os.path.join(out_path_monthly, 'Power_generation_for_all_country_%s.svg' % current_date), format='svg')
-# readme也需要一个
-plt.savefig(os.path.join(out_path, 'Power_generation_for_all_country.svg'), format='svg')
+    # 前一天的数据基本都不准确
+    yesterday = af.get_yesterday().strftime('%Y-%m-%d')
+    df_all = df_all[df_all['date'] < yesterday].reset_index(drop=True)
+
+    df_all = df_all[['date', 'country', 'Type', 'Value']]
+
+    if not category:
+        df_all = df_all.groupby(['date', 'country']).sum().reset_index()
+        df_all['Type'] = 'All'
+    else:
+        df_all = df_all[df_all['Type'] == category].reset_index(drop=True)
+    df_all = df_all.rename(columns={'country': 'country/region', 'Type': 'energy_source', 'Value': 'value'})
+
+    # 添加欧盟
+    df_c = pd.read_csv(os.path.join(global_path, 'EU_country_list.csv'))
+
+    eu27_list = df_c['country'].tolist() + ['United Kingdom']
+    df_eu27 = df_all[df_all['country/region'].isin(eu27_list)].groupby(
+        ['date', 'energy_source']).sum().reset_index()
+    df_eu27['country/region'] = 'EU27&UK'
+    df_all = pd.concat([df_all, df_eu27]).reset_index(drop=True)
+
+    df_all = df_all.groupby(['country/region', 'date']).sum().reset_index()
+    df_all = pd.pivot_table(df_all, index='date', values='value', columns='country/region').reset_index()
+
+    df_all = df_all[['date'] + country_list]
+    df_all = df_all[df_all['date'] >= '2019-01-01'].reset_index(drop=True)
+    df_all['year'] = df_all['date'].dt.year
+    df_all['month'] = df_all['date'].dt.month
+    return df_all
+
+
+def draw_pic(df_all, c, i):
+    # 开始画图
+    year_list = df_all['year'].drop_duplicates().tolist()
+    n = 0.3
+    color_pool = []
+    for z in range(len(year_list)):  # 设置渐变颜色
+        color_pool.append(n)
+        n += 0.4
+    # plt.style.use('seaborn')
+
+    size_num = 70
+    plt.title(c, size=size_num)
+    if i == 0 or i == 4 or i == 8:  # 这里以后要修改
+        plt.ylabel('Power generated (Gwh)', size=size_num)
+    else:
+        plt.ylabel('')
+
+    for d, p in zip(range(len(year_list)), color_pool):
+        x = df_all[df_all['year'] == year_list[0]]['date'].tolist()
+        y = df_all[df_all['year'] == year_list[d]][c].tolist()[0:len(x)]
+        # noinspection PyBroadException
+        try:
+            plt.plot(x, y, color=af.lighten_color('orange', p), linewidth=8, label=year_list[d])
+        except:  # 如果长度不一致
+            len_num = len(x) - len(y)
+            y = y + [None] * len_num
+            plt.plot(x, y, color=af.lighten_color('grey', p), linewidth=8, label=year_list[d])
+    ax = plt.gca()  # 表明设置图片的各个轴，plt.gcf()表示图片本身
+    ax.xaxis.set_major_locator(MonthLocator())
+    ax.xaxis.set_major_formatter(DateFormatter('%b'))
+    plt.legend(loc='best', prop={'size': 80})
+    plt.yticks(size=40)
+    if i <= 7:  # 这里以后要修改
+        plt.xticks(())
+    else:
+        plt.xticks(size=60)
+
+
+def sub_plot(df_all, country_list):
+    for i in range(len(country_list)):
+        plt.subplot(3, 4, i + 1)
+        draw_pic(df_all, country_list[i], i)
+    plt.tight_layout()
+
+
+def out_put(out_path, category):
+    current_date = datetime.now().strftime('%Y%m%d')
+    if not category:  # 全部的情况
+        out_path_type = af.create_folder(out_path, 'all')
+        out_path_yearly = af.create_folder(out_path_type, current_date[:4])
+        out_path_monthly = af.create_folder(out_path_yearly, current_date[4:6])
+
+        plt.savefig(os.path.join(out_path_monthly, 'Power_generation_for_all_country_%s.svg' % current_date),
+                    format='svg')
+        # readme也需要一个
+        plt.savefig(os.path.join(out_path, 'Power_generation_for_all_country.svg'), format='svg')
+    else:
+        out_path_type = af.create_folder(out_path, category)
+        out_path_yearly = af.create_folder(out_path_type, current_date[:4])
+        out_path_monthly = af.create_folder(out_path_yearly, current_date[4:6])
+
+        plt.savefig(os.path.join(out_path_monthly, 'Power_generation_for_all_country_%s.svg' % current_date),
+                    format='svg')
+        plt.savefig(os.path.join(out_path, '%s_generation_for_all_country.svg' % category.capitalize()), format='svg')
+
+
+# if __name__ == '__main__':
+#     main(category)
+    # main(category='gas')
+    # main(category='oil')
+    # main(category='nuclear')
+    # main(category='solar')
+    # main(category='hydro')
+    # main(category='wind')
+    # main(category='other')
+    # main(category=False)
+
