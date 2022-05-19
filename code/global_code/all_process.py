48d7f60f3a6a97cdb89682c45d18a2afb2d65267
KowComical <xuanren1992@yahoo.com>
2022-05-19 18:27:56 +0800

    Update all_process.py

--- code/global_code/all_process.py
+++ code/global_code/all_process.py
@@ -12,75 +12,75 @@ try:
 except Exception as e:
     print(e)
 
-try:
-    sys.path.append('./code/asian/china/')
-    print('Begin process China...')
-    import china_workflow as c
-    c.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/asian/india/')
-    print('Begin process India...')
-    import india_workflow as i
-    i.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/asian/japan/')
-    print('Begin process Japan...')
-    import japan_workflow as j
-    j.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/europe/eu27_uk/')
-    print('Begin process EU...')
-    import eu_workflow as e
-    e.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/europe/russia/')
-    print('Begin process Russia...')
-    import russia_workflow as r
-    r.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/n_america/us/')
-    print('Begin process US...')
-    import us_workflow as us
-    us.main()
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/s_america/brazil/')
-    print('Begin process Brazil...')
-    import brazil_workflow as b
-    b.main()
-except Exception as e:
-    print(e)
-
-try:
-    print('Begin process iea...')
-    import iea_workflow as iea
-    iea.main()
-except Exception as e:
-    print(e)
-
-try:
-    print('Begin process bp...')
-    import bp_workflow as bp
-    bp.main()
-except Exception as e:
-    print(e)
+# try:
+#     sys.path.append('./code/asian/china/')
+#     print('Begin process China...')
+#     import china_workflow as c
+#     c.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/asian/india/')
+#     print('Begin process India...')
+#     import india_workflow as i
+#     i.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/asian/japan/')
+#     print('Begin process Japan...')
+#     import japan_workflow as j
+#     j.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/europe/eu27_uk/')
+#     print('Begin process EU...')
+#     import eu_workflow as e
+#     e.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/europe/russia/')
+#     print('Begin process Russia...')
+#     import russia_workflow as r
+#     r.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/n_america/us/')
+#     print('Begin process US...')
+#     import us_workflow as us
+#     us.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/s_america/brazil/')
+#     print('Begin process Brazil...')
+#     import brazil_workflow as b
+#     b.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     print('Begin process iea...')
+#     import iea_workflow as iea
+#     iea.main()
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     print('Begin process bp...')
+#     import bp_workflow as bp
+#     bp.main()
+# except Exception as e:
+#     print(e)
 
 import cal_ef_emission as cal
 cal.main()
