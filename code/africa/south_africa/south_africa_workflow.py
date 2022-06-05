856c62f0aeaadb83edba198c0cd63fbc18008004
KowComical <xuanren1992@yahoo.com>
2022-06-05 14:21:23 +0800

    Update south_africa_workflow.py

--- code/africa/south_africa/south_africa_workflow.py
+++ code/africa/south_africa/south_africa_workflow.py
@@ -13,7 +13,7 @@ import os
 import sys
 
 sys.dont_write_bytecode = True
-sys.path.append('./code/')
+sys.path.append('./code/global_code/')
 from global_code import global_function as af
 from global_code import global_all as g
 
