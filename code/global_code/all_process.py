f44180e30e5064b5db05f5218339394a59161229
KowComical <xuanren1992@yahoo.com>
2022-06-07 10:37:54 +0800

    Update all_process.py

--- code/global_code/all_process.py
+++ code/global_code/all_process.py
@@ -8,6 +8,7 @@ try:
     print('Begin process South Africa...')
     import south_africa.south_africa_workflow as sa
     sa.main()
+    print('Finish process South Africa...')
 except Exception as e:
     print(e)
 
@@ -16,6 +17,7 @@ try:
     print('Begin process China...')
     import china.china_workflow as c
     c.main()
+    print('Finish process China...')
 except Exception as e:
     print(e)
 
@@ -24,6 +26,7 @@ try:
     print('Begin process India...')
     import india.india_workflow as i
     i.main()
+    print('Finish process India...')
 except Exception as e:
     print(e)
 
@@ -32,6 +35,7 @@ try:
     print('Begin process Japan...')
     import japan.japan_workflow as j
     j.main()
+    print('Finish process Japan...')
 except Exception as e:
     print(e)
 
@@ -40,6 +44,7 @@ try:
     print('Begin process EU...')
     import eu27_uk.eu_workflow as eu
     eu.main()
+    print('Finish process EU...')
 except Exception as e:
     print(e)
 
@@ -48,6 +53,7 @@ try:
     print('Begin process Russia...')
     import russia.russia_workflow as r
     r.main()
+    print('Finish process Russia...')
 except Exception as e:
     print(e)
 
@@ -56,6 +62,7 @@ try:
     print('Begin process US...')
     import us.us_workflow as us
     us.main()
+    print('Finish process US...')
 except Exception as e:
     print(e)
 
@@ -64,6 +71,7 @@ try:
     print('Begin process Brazil...')
     import brazil.brazil_workflow as b
     b.main()
+    print('Finish process Brazil...')
 except Exception as e:
     print(e)
 
@@ -71,6 +79,7 @@ try:
     print('Begin process iea...')
     import iea_workflow as iea
     iea.main()
+    print('Finish process iea...')
 except Exception as e:
     print(e)
 
@@ -78,6 +87,7 @@ try:
     print('Begin process bp...')
     import bp_workflow as bp
     bp.main()
+    print('Finish process bp...')
 except Exception as e:
     print(e)
 
@@ -89,7 +99,9 @@ cal.main()
 print('Begin process draw_pic...')
 import draw_pic as d
 
+
 energy_list = ['coal', 'gas', 'oil', 'nuclear', 'hydro', 'solar', 'wind', 'other']
 for e in energy_list:
     d.main(category=e)
 d.main(category=False)
+print('Finish process draw_pic...')
