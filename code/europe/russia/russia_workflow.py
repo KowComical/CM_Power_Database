864afce3b43c3a1b5350c3913f27a9d5fd9c527c
KowComical <xuanren1992@yahoo.com>
2022-06-16 18:34:18 +0800

    update russia

--- code/europe/russia/russia_workflow.py
+++ code/europe/russia/russia_workflow.py
@@ -25,6 +25,30 @@ def craw():
     url = 'https://emres.cn/api/carbonmonitor/getRussiaPowerHourly.php'
     r = requests.get(url)
     df = pd.json_normalize(r.json())
+
+    # 校准数据
+    df['M_DATE'] = pd.to_datetime(df['M_DATE'], format='%d.%m.%Y 0:00:00')
+    # 2016-01-01 ~ 2020-03-23 的所有thermal数据乘以1.8
+    index_list = df[(df['M_DATE'] >= '2016-01-01') & (df['M_DATE'] <= '2020-03-23')].index.tolist()
+    for t in index_list:
+        df.loc[t, 'P_TES'] = df.loc[t, 'P_TES'] * 1.8
+    # 2016-05-01 ~ 2016-09-18 的thermal数据再乘以1.3
+    index_list = df[(df['M_DATE'] >= '2016-05-01') & (df['M_DATE'] <= '2016-09-18')].index.tolist()
+    for t in index_list:
+        df.loc[t, 'P_TES'] = df.loc[t, 'P_TES'] * 1.3
+    # 2017-05-01 ~ 2017-09-18 的thermal数据再乘以1.3
+    index_list = df[(df['M_DATE'] >= '2017-05-01') & (df['M_DATE'] <= '2017-09-18')].index.tolist()
+    for t in index_list:
+        df.loc[t, 'P_TES'] = df.loc[t, 'P_TES'] * 1.3
+    # 2018-05-01 ~ 2018-09-18 的thermal数据再乘以1.3
+    index_list = df[(df['M_DATE'] >= '2018-05-01') & (df['M_DATE'] <= '2018-09-18')].index.tolist()
+    for t in index_list:
+        df.loc[t, 'P_TES'] = df.loc[t, 'P_TES'] * 1.3
+    # 2019-05-01 ~ 2019-09-18 的thermal数据再乘以1.3
+    index_list = df[(df['M_DATE'] >= '2019-05-01') & (df['M_DATE'] <= '2019-09-18')].index.tolist()
+    for t in index_list:
+        df.loc[t, 'P_TES'] = df.loc[t, 'P_TES'] * 1.3
+
     df.to_csv(os.path.join(out_path, 'Russia_Hourly_Generation.csv'), index=False, encoding='utf_8_sig')
 
 
