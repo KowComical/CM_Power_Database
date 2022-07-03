30d76267e7c2819dcb3719767494848fee067461
KowComical <xuanren1992@yahoo.com>
2022-07-03 19:43:56 +0800

    update japan

--- code/oceania/australia/australia_craw.py
+++ code/oceania/australia/australia_craw.py
@@ -2,18 +2,20 @@ import requests
 import pandas as pd
 import os
 import time
+import re
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 import sys
+
 sys.dont_write_bytecode = True
 sys.path.append('./code/')
 from global_code import global_function as af
 
 file_path = './data/oceania/australia/craw/'
 
-end_date = datetime.now().strftime('%Y-%m')
+end_date = (datetime.now() + relativedelta(months=1)).strftime('%Y-%m')  # 爬到这个日期截至
 region_list = ['NEM/NSW1', 'NEM/QLD1', 'NEM/SA1', 'NEM/TAS1', 'NEM/VIC1', 'WEM/WEM']
-
 
 # 所有已爬取的
 all_file = af.search_file(file_path)
@@ -27,25 +29,32 @@ def main():
 
 
 def craw():
-    for re in region_list:
-        if re == 'NEM/TAS1':
-            start_date = '2006-01'
-        elif re == 'WEM/WEM':
-            start_date = '2015-01'
-        else:
-            start_date = '1999-01'
-        data_range = pd.date_range(start=start_date, end=end_date, freq='1M')
-        url = 'https://api.opennem.org.au/stats/power/network/fueltech/%s' % re
-        re = re.replace('/', '_')  # windows文件名不能有斜杠 所以要转为下划线
-        out_path = os.path.join(file_path, re)
+    for RE in region_list:
+        re_windows = RE.replace('/', '_')  # windows文件名不能有斜杠 所以要转为下划线
+        out_path = os.path.join(file_path, re_windows)
         if not os.path.exists(out_path):  # 如果有了文件夹的话就直接pass掉
             os.mkdir(out_path)
+        # 找出起始日期
+        file_name = af.search_file(out_path)
+        date_name = re.compile(r'month/(?P<name>.*?)_', re.S)  # 从路径找出日期
+        date = [date_name.findall(f)[0] for f in file_name]
+        start_date = max(date)
+        if not start_date:  # 如果还未爬取过
+            if RE == 'NEM/TAS1':
+                start_date = '2006-01'
+            elif RE == 'WEM/WEM':
+                start_date = '2015-01'
+            else:
+                start_date = '1999-01'
+
+        data_range = pd.date_range(start=start_date, end=end_date, freq='1M')
+        url = 'https://api.opennem.org.au/stats/power/network/fueltech/%s' % RE
 
         for d in data_range:
             params_data = {'month': pd.to_datetime(d).strftime('%Y-%m-%d')}
             # 如果已爬取则略过
             d_name = pd.to_datetime(d).strftime('%Y-%m')
-            exist_name = os.path.join(file_path, re, '%s.csv' % d_name)
+            exist_name = os.path.join(file_path, RE, '%s.csv' % d_name)
             if exist_name not in all_file:
                 r_t = requests.get(url, params=params_data, timeout=60)
                 result = pd.json_normalize(r_t.json(), record_path='data')
